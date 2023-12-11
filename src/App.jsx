835ab6d7b13a58ae8cf006bc3c2cac1759ca172a
izleenfaqihah <izleenfaqihah@gmail.com>
2023-12-11 18:21:30 +0800

    main

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Homepage from './component/Homepage'
+
+const App = () => {
+  return (
+    <Homepage/>
+  )
+}
+
+export default App
---
+++ src/component/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { MDBBtn, MDBContainer } from "mdb-react-ui-kit";
+import "./Homepage.css"
+
+
+const Homepage = () => {
+  return (
+    <section>
+      <h5>MFM Shopping Website</h5>
+          <h2>
+            <span>SHOP</span><span>HERE</span>
+          </h2>
+    </section>
+  );
+};
+
+export default Homepage;
